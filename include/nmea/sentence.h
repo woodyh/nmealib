f2590e5e2e9a69a77d5b44dbcb3fb44f1202f814
Ferry Huberts <f.huberts@mindef.nl>
2012-07-11 13:32:47 +0200

    sentence: fix a comment
    Signed-off-by: Ferry Huberts <f.huberts@mindef.nl>

--- include/nmea/sentence.h
+++ include/nmea/sentence.h
@@ -59,15 +59,15 @@ enum nmeaPACKTYPE {
  *      123519       Fix taken at 12:35:19 UTC
  *      4807.038,N   Latitude 48 deg 07.038' N
  *      01131.000,E  Longitude 11 deg 31.000' E
- *      1            Fix quality: 0 = invalid
- *                                1 = GPS fix (SPS)
- *                                2 = DGPS fix
- *                                3 = PPS fix
- * 			                      4 = Real Time Kinematic
- * 			                      5 = Float RTK
- *                                6 = estimated (dead reckoning) (2.3 feature)
- * 			                      7 = Manual input mode
- * 			                      8 = Simulation mode
+ *      1            Signal quality: 0 = invalid
+ *                                   1 = GPS fix (SPS)
+ *                                   2 = DGPS fix
+ *                                   3 = PPS fix
+ * 			                         4 = Real Time Kinematic
+ * 			                         5 = Float RTK
+ *                                   6 = estimated (dead reckoning) (2.3 feature)
+ * 			                         7 = Manual input mode
+ * 			                         8 = Simulation mode
  *      08           Number of satellites being tracked
  *      0.9          Horizontal dilution of position
  *      545.4,M      Altitude, Meters, above mean sea level
