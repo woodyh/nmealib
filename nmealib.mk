3007afad2aa297965e0ee0e7849d4eb9ac510aa4
Woody Hawthorne <erhawthorne@gmail.com>
2015-11-02 23:29:54 +1100

    Added makefile.

---
+++ nmealib.mk
@@ -0,0 +1,9 @@
+NMEAINC = 	$(NMEALIB)/include
+
+NMEASRC = 	$(NMEALIB)/src/context.c \
+			$(NMEALIB)/src/conversions.c \
+			$(NMEALIB)/src/gmath.c \
+			$(NMEALIB)/src/info.c \
+			$(NMEALIB)/src/parse.c \
+			$(NMEALIB)/src/parser.c \
+			$(NMEALIB)/src/tok.c
