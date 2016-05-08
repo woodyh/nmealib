4e78d0aeced4e59e2a322e3c618a55460eed0c5c
Woody Hawthorne <erhawthorne@gmail.com>
2016-05-08 21:55:12 +1000

    Removed 'samples' and restructured files.

--- nmealib.mk
+++ nmealib.mk
@@ -1,8 +1,8 @@
 NMEAINC = 	$(NMEALIB)/include
 
 NMEASRC = 	$(NMEALIB)/src/conversions.c \
-			$(NMEALIB)/src/gmath.c \
-			$(NMEALIB)/src/info.c \
-			$(NMEALIB)/src/parse.c \
-			$(NMEALIB)/src/parser.c \
-			$(NMEALIB)/src/tok.c
+		$(NMEALIB)/src/gmath.c \
+		$(NMEALIB)/src/info.c \
+		$(NMEALIB)/src/parse.c \
+		$(NMEALIB)/src/parser.c \
+		$(NMEALIB)/src/tok.c
